9c6376ca0559b582a58c417f9f2f1e465ceaa3e1
Thatcher Peskens <thatcher@docker.com>
2013-12-02 21:33:25 +0000

    renamed exec to dcr (docker container runner), syntax improvements

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='dcs',
+   version='0.1.1',
+   description='Docker Container Start - start Docker Containers from YML',
+   url='http://github.com/dhrp/docker-runner',
+   author='Thatcher Peskens',
+   author_email='thatcher@docker.com',
+   license='MIT',
+#   packages=['docker_container_start'],
+   scripts=['dcs'],
+   install_requires=[
+      'docker-py==0.2.2',
+      'simplejson',
+      'jsonpath_rw'
+   ],
+   dependency_links=[
+      'https://github.com/dotcloud/docker-py/tarball/master#egg=docker-py-0.2.2'
+   ],
+   zip_safe=False)
+
